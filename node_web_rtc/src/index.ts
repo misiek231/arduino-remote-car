e4a8ad995f013bf812dca74e67423eb29b0d05d6
Michał Woźniak <m.wozniak@ideo.pl>
2025-04-05 22:56:21 +0200

    fix: deleted unneeded endpoint

--- node_web_rtc/src/index.ts
+++ node_web_rtc/src/index.ts
@@ -53,10 +53,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.get('/hello', (req, res) => {
-    res.send("siema");
-});
-
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
